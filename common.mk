Ashwin4RC/android_device_oneplus_sm8150-common

sm8150-common: adapt namespaces for posp

common.mk
@@ -332,6 +332,11 @@ PRODUCT_PACKAGES += \
     libchrome \
     libchrome.vendor
 
+# Namespace
+PRODUCT_BOARD_PLATFORM := sm8150
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0

PotatoDevices/device_oneplus_sm8150-common

common.mk
@@ -328,6 +328,11 @@ PRODUCT_PACKAGES += \
     libchrome \
     libchrome.vendor
 
+# Namespace
+PRODUCT_BOARD_PLATFORM := msmnile
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0